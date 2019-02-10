077cc6b22bb870013b9b37f10256a3bae547c2da
marvokdolor <marvourneen@mkdolor.com>
2019-02-10 17:53:52 -0500

    Updates

---
+++ precache-manifest.71f80d7318b694d3d71fec0fcfb2d48c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "46d1469ea60f852e911f",
+    "url": "/pomodoro-clock-fcc/static/js/runtime~main.46d1469e.js"
+  },
+  {
+    "revision": "1dac6da0c5769e4efae7",
+    "url": "/pomodoro-clock-fcc/static/js/main.1dac6da0.chunk.js"
+  },
+  {
+    "revision": "b37b0e24dc7a40cb64e3",
+    "url": "/pomodoro-clock-fcc/static/js/1.b37b0e24.chunk.js"
+  },
+  {
+    "revision": "1dac6da0c5769e4efae7",
+    "url": "/pomodoro-clock-fcc/static/css/main.2a6055c6.chunk.css"
+  },
+  {
+    "revision": "ad97e68b0732c9ff6ff5defbdebc0769",
+    "url": "/pomodoro-clock-fcc/index.html"
+  }
+];
